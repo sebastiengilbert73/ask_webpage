c83f74c546ae468f6fb620aa33577303d8ad428a
GMX\070038649 <segilber@ca.ibm.com>
2024-01-19 12:08:21 -0500

    .\src\ask_webpage\local_llm.py .\test\test_local_llm.py: 1st commit

---
+++ src/ask_webpage/local_llm.py
@@ -0,0 +1,26 @@
+from llama_index.llms import HuggingFaceLLM
+from transformers import BitsAndBytesConfig
+from llama_index.prompts import PromptTemplate
+
+def zephyr_7b_alpha(context_window=2048, max_new_tokens=256):
+    query_wrapper_prompt = PromptTemplate("<|system|>\n</s>\n<|user|>\n{query_str}</s>\n<|assistant|>\n")
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_use_double_quant=True,
+    )
+    llm = HuggingFaceLLM(
+        model_name="HuggingFaceH4/zephyr-7b-alpha",
+        tokenizer_name="HuggingFaceH4/zephyr-7b-alpha",
+        query_wrapper_prompt=query_wrapper_prompt,
+        context_window=context_window,
+        max_new_tokens=max_new_tokens,
+        model_kwargs={"quantization_config": quantization_config, "trust_remote_code": True},
+        # tokenizer_kwargs={},
+        generate_kwargs={"do_sample": False},
+        messages_to_prompt=messages_to_prompt,
+        device_map="auto"
+    )
+
+    return llm
---
+++ test/test_local_llm.py
@@ -0,0 +1,14 @@
+import logging
+sys.path.append("../src/ask_webpage")
+import local_llm
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)s \t%(message)s')
+
+def main():
+    logging.debug("test_local_llm.main()")
+
+    llm = local_llm.zephyr_7b_alpha()
+
+
+if __name__ == '__main__':
+    main()
