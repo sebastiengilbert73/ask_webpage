baa3203a136e743ff126f9f2693fe363f8d28be2
GMX\070038649 <segilber@ca.ibm.com>
2024-01-19 14:46:01 -0500

    .\src\ask_webpage\webpage.py: Accepts list of urls

--- src/ask_webpage/webpage.py
+++ src/ask_webpage/webpage.py
@@ -2,9 +2,8 @@ from llama_index import download_loader
 SimpleWebPageReader = download_loader("SimpleWebPageReader")
 
 class WebpageData():
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, urls):
+        self.urls = urls
         self.loader = SimpleWebPageReader()
-        self.document = self.loader.load_data(urls=[url])[0]
+        self.documents = self.loader.load_data(urls=self.urls)
 
-    def encode(self, embedder):
