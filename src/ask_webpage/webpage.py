453f8deee49e52b89b5228d5fff47a9cd8e3b617
GMX\070038649 <segilber@ca.ibm.com>
2024-01-19 13:33:26 -0500

    .\src\ask_webpage\webpage.py: Adds headers

--- src/ask_webpage/webpage.py
+++ src/ask_webpage/webpage.py
@@ -4,6 +4,7 @@ UnstructuredURLLoader = download_loader("UnstructuredURLLoader")
 class WebpageData():
     def __init__(self, url):
         self.url = url
-        self.loader = UnstructuredURLLoader(urls=[url], continue_on_failure=False)
+        self.loader = UnstructuredURLLoader(urls=[url], continue_on_failure=False,
+                                            headers={"User-Agent": "value"})
         self.loader.load()
 
