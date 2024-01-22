d05e76b9b0d87eb94a5cd2885215978bff4cf4b7
GMX\070038649 <segilber@ca.ibm.com>
2024-01-22 16:10:11 -0500

    .\gradio_server\app.py: If website_url is already in the db, don't load again

--- gradio_server/app.py
+++ gradio_server/app.py
@@ -38,17 +38,24 @@ def search_website(website_url, query):
     webpage_extractor = webpage.WebpageData([website_url])
     documents = webpage_extractor.documents
 
-    db_name = "webpage"
     db = chromadb.Client()
-    chroma_collection = db.create_collection(db_name)
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-    vs_index = VectorStoreIndex.from_documents(
-        documents,
-        service_context=service_context,
-        storage_context=storage_context,
-        show_progress=True
-    )
+    if website_url not in [c.name for c in db.list_collections()]:  # Create db
+        chroma_collection = db.create_collection(website_url)
+        vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        vs_index = VectorStoreIndex.from_documents(
+            documents,
+            service_context=service_context,
+            storage_context=storage_context,
+            show_progress=True
+        )
+    else:  # db already exists
+        chroma_collection = db.get_collection(website_url)
+        vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+        vs_index = VectorStoreIndex.from_vector_store(
+            vector_store=vector_store,
+            service_context=service_context
+        )
 
     ret = vs_index.as_retriever(similarity_top_k=5)
 
